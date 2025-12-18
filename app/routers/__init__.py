fcb1aea3971281ac826c0d08dd926ee711344d00
dx <1193355375@qq.com>
2025-12-19 00:22:25 +0800

    feat: add tasks router to the main router module for improved task management integration

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,6 +3,13 @@ from .chat import router as chat_router
 from .recognize import router as recognize_router
 from .search import router as search_router
 from .plan import router as plan_router
+from .tasks import router as tasks_router
 
-__all__ = ["chat_router", "recognize_router", "search_router", "plan_router"]
+__all__ = [
+    "chat_router",
+    "recognize_router",
+    "search_router",
+    "plan_router",
+    "tasks_router",
+]
 
